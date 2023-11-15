0df6267e5188593b83da0163cb1f18405bc54ac2
Heesu Ban <ebandal@gmail.com>
2023-11-15 12:45:50 +0900

    add decode methods

--- src/run/ReadTest.java
+++ src/run/ReadTest.java
@@ -1,10 +1,16 @@
 package run;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import decode.DwgParseException;
 import structure.Dwg;
@@ -28,4 +34,47 @@ public class ReadTest {
         }
     }
 
+    public static void classify( ) {
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(Level.ALL);
+        for (Handler h: rootLogger.getHandlers()) {
+            h.setLevel(Level.ALL);
+        }
+        
+        try {
+            Stream<Path> streams = Files.walk(Paths.get("DWG"));
+            streams.filter(p -> p.toFile().isFile())
+                    .forEach(p -> {
+                        Dwg dwg = new Dwg();
+                        try {
+                            dwg.decode(p.toFile());
+                            switch(dwg.header.versionId) {
+                            case "AC1012":  moveToFolder(p, "AC1012");  break;
+                            case "AC1014":  moveToFolder(p, "AC1014");  break;
+                            case "AC1015":  moveToFolder(p, "AC1015");  break;
+                            case "AC1018":  moveToFolder(p, "AC1018");  break;
+                            case "AC1021":  moveToFolder(p, "AC1021");  break;
+                            case "AC1024":  moveToFolder(p, "AC1024");  break;
+                            case "AC1027":  moveToFolder(p, "AC1027");  break;
+                            case "AC1032":  moveToFolder(p, "AC1032");  break;
+                            default:        moveToFolder(p, "FAIL");    break;
+                            }
+                        } catch (DwgParseException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void moveToFolder(Path p, String folderName) {
+        try {
+            Files.createDirectories(Paths.get(folderName), null);
+            Path target = Paths.get(folderName, p.getFileName().toString());
+            Files.move(p,  target,  StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/structure/Dwg.java
+++ src/structure/Dwg.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.List;
 import java.util.logging.Logger;
 
+import decode.DecodeCallback;
 import decode.DecoderR14;
 import decode.DwgParseException;
 import structure.header.FileHeader;
@@ -97,17 +101,28 @@ public class Dwg {
             offset += DecoderR14.readFileHeader(raf, offset, fileHeader);
             break;
         case "AC1015":
-            fileHeader.ver = DwgVersion.R2000; break;
+            fileHeader.ver = DwgVersion.R2000;
+            offset += DecoderR14.readFileHeader(raf,  offset, fileHeader);
+            break;
         case "AC1018":
-            fileHeader.ver = DwgVersion.R2004; break;
+            fileHeader.ver = DwgVersion.R2004;
+            buf = new byte[0x100];
+            offset += 6;
+            readLen = raf.read(buf, offset, 0x100-6);
+            // offset += DecoderR2004.readFileHeader(buf, offset, fileHeader);
+            break;
         case "AC1021":
-            fileHeader.ver = DwgVersion.R2007; break;
+            fileHeader.ver = DwgVersion.R2007;
+            break;
         case "AC1024":
-            fileHeader.ver = DwgVersion.R2010; break;
+            fileHeader.ver = DwgVersion.R2010;
+            break;
         case "AC1027":
-            fileHeader.ver = DwgVersion.R2013; break;
+            fileHeader.ver = DwgVersion.R2013;
+            break;
         case "AC1032":
-            fileHeader.ver = DwgVersion.R2018; break;
+            fileHeader.ver = DwgVersion.R2018;
+            break;
         }
         
         // six bytes of 0 (in R14, 5 0's and the ACADMAINTVER)
@@ -127,9 +142,257 @@ public class Dwg {
         return 0;
     }
 
-    public static HeaderVariables readHeaderVariable(byte[] buf, int offset, DwgVersion ver) {
+    public static HeaderVariables readHeaderVariable(byte[] buf, int off, DwgVersion ver) {
+        AtomicInteger offset = new AtomicInteger(off);
+        AtomicInteger bitOffset = new AtomicInteger(0);
+        int retBitOffset = 0;
+        
+        DecodeCallback cb = new DecodeCallback() {
+            public void onDecoded(String name, Object value, int retBitOffset) {
+                log.info("[" + name + "] = (" + value.toString() + ")");
+                offset.addAndGet((bitOffset.get()+retBitOffset)/8);
+                bitOffset.set((bitOffset.get()+retBitOffset)%8);
+            }
+        };
+        
+        // beginning sentinel
+        // 
+        offset.addAndGet(16);
+        
+        // Size of the section (a 4 byte long)
+        int size = ByteBuffer.wrap(buf, offset.get(), 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        offset.addAndGet(4);
+        
+        // R2010/R1013 (only present if the maintenance version is greater than 3!) or R2018_:
+        //  unknown (4 byte long), might be part of a 62-bit size.
+        if (ver.between(DwgVersion.R2010,  DwgVersion.R2013)) {
+            
+        } else if (ver.from(DwgVersion.R2018)) {
+            offset.addAndGet(4);
+        }
+        
+        HeaderVariables hdrVars = new HeaderVariables();
+        // DATA
+        // R2007 Only:
+        //  RL : Size in bits
+        long sizeInBits = 0;
+        if (ver.only(DwgVersion.R2007)) {
+            hdrVars.lSizeInBits = readRawLong(buf, offset.get(), bitOffset.get(), "SizeInBits", cb);
+        }
+        
+        return hdrVars;
+    }
+    
+    private static double readBitDouble(byte[] buf, int off, int bitOff, String name, DecodeCallback cb) {
+        int retBitOffset = 0;
+        int offset = off;
+        int bitOffset = bitOff;
+        byte[] dBuf = new byte[8];
+        double value = 0.0;
+        
+        byte bitControl = bitOffset==7 ? (byte)((buf[offset]<<1 & 0x02)|(buf[offset+1]>>7 & 0x01)) : (byte)(buf[offset]>>(8-bitOffset-2) & 0x03);
+        bitOffset += 2; retBitOffset += 2;
+        offset += bitOffset/8;
+        bitOffset = bitOffset%8;
+        
+        switch(bitControl) {
+        case 0: // A double follows
+            for (int i=0; i<8; i++) {
+                dBuf[i] = (byte) ((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+            }
+            long lValue = ByteBuffer.wrap(buf, 0, 8).order(ByteOrder.LITTLE_ENDIAN).getLong();
+            value = Double.longBitsToDouble(lValue);
+            offset += 8; retBitOffset += 64;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 1: // 1.0
+            value = 1.0;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 2: // 0.0
+            value = 0.0;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 3: // not used
+            break;
+        }
+        
+        return value;
+    }
+    
+    private static String readVariableText(byte[] buf, int off, int bitOff, DwgVersion ver, String name, DecodeCallback cb) {
+        if (ver.until(DwgVersion.R2004)) {
+            return readText(buf, off, bitOff, name, cb);
+        } else {
+            return readUnicodeText(buf, off, bitOff, name, cb);
+        }
+    }
+    
+    private static String readText(byte[] buf, int off, int bitOff, String name, DecodeCallback cb) {
+        int retBitOffset = 0;
+        int offset = off;
+        int bitOffset = bitOff;
+        byte[] dBuf = new byte[2];
+        short textLength = 0;
+        
+        byte bitControl = bitOffset==7 ? (byte)((buf[offset]<<1 & 0x02)|(buf[offset+1]>>7 & 0x01)) : (byte)(buf[offset]>>(8-bitOffset-2) & 0x03);
+        bitOffset += 2; retBitOffset += 2;
+        offset += bitOffset/8;
+        bitOffset = bitOffset%8;
+        
+        // text (bitshort length, followed by the string)
+        switch(bitControl) {
+        case 0: // A short (2 bytes) follows, little-endian order (LSB first)
+            for (int i=0; i<2; i++) {
+                dBuf[i] = (byte) ((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+            }
+            offset += 2; retBitOffset += 16;
+            textLength = ByteBuffer.wrap(dBuf, 0, 2).order(ByteOrder.LITTLE_ENDIAN).getShort();
+            break;
+        case 1: // An unsigned char (1 byte) follows
+            textLength = (short) ((((buf[offset+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset]<<bitOffset&0xFF));
+            offset += 1; retBitOffset += 8;
+            break;
+        case 2: // 0
+            textLength = 0;
+            break;
+        case 3: // 256
+            textLength = 256;
+            break;
+        }
+        
+        dBuf = new byte[textLength];
+        for (int i=0; i<textLength; i++) {
+            dBuf[i] = (byte)((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+        }
+        offset += textLength; retBitOffset += (textLength*8);
+        String value = new String(dBuf, 0, textLength, StandardCharsets.US_ASCII);
+        cb.onDecoded(name, value, retBitOffset);
+        
+        return value;
+    }
+    
+    private static String readUnicodeText(byte[] buf, int off, int bitOff, String name, DecodeCallback cb) {
+        int retBitOffset = 0;
+        int offset = off;
+        int bitOffset = bitOff;
+        byte[] dBuf = new byte[2];
+        short textLength = 0;
+        
+        byte bitControl = bitOffset==7 ? (byte)((buf[offset]<<1 & 0x02)|(buf[offset+1]>>7 & 0x01)) : (byte)(buf[offset]>>(8-bitOffset-2) & 0x03);
+        bitOffset += 2; retBitOffset += 2;
+        offset += bitOffset/8;
+        bitOffset = bitOffset%8;
+        
+        // text (bitshort length, followed by the string)
+        switch(bitControl) {
+        case 0: // A short (2 bytes) follows, little-endian order (LSB first)
+            for (int i=0; i<2; i++) {
+                dBuf[i] = (byte) ((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+            }
+            offset += 2; retBitOffset += 16;
+            textLength = ByteBuffer.wrap(dBuf, 0, 2).order(ByteOrder.LITTLE_ENDIAN).getShort();
+            break;
+        case 1: // An unsigned char (1 byte) follows
+            textLength = (short) ((((buf[offset+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset]<<bitOffset&0xFF));
+            offset += 1; retBitOffset += 8;
+            break;
+        case 2: // 0
+            textLength = 0;
+            break;
+        case 3: // 256
+            textLength = 256;
+            break;
+        }
+        
+        dBuf = new byte[textLength];
+        for (int i=0; i<textLength; i++) {
+            dBuf[i] = (byte)((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+        }
+        offset += textLength; retBitOffset += (textLength*8);
+        String value = new String(dBuf, 0, textLength, StandardCharsets.UTF_16LE);
+        cb.onDecoded(name, value, retBitOffset);
+        
+        return value;
+    }
+    
+    public static int readBitLong(byte[] buf, int off, int bitOff, String name, DecodeCallback cb) {
+        int retBitOffset = 0;
+        int offset = off;
+        int bitOffset = bitOff;
+        byte[] dBuf = new byte[4];
+        int value = 0;
+        
+        byte bitControl = bitOffset==7 ? (byte)((buf[offset]<<1 & 0x02)|(buf[offset+1]>>7 & 0x01)) : (byte)(buf[offset]>>(8-bitOffset-2) & 0x03);
+        bitOffset += 2; retBitOffset += 2;
+        offset += bitOffset/8;
+        bitOffset = bitOffset%8;
+        
+        switch(bitControl) {
+        case 0: // A long (4bytes) follows, little-endian order (LSB first)
+            for (int i=0; i<4; i++) {
+                dBuf[i] = (byte) ((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+            }
+            value = ByteBuffer.wrap(dBuf, 0, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            offset += 4; retBitOffset += 32;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 1: // An unsgined char (1 byte) follow
+            value = ((((buf[offset+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset]<<bitOffset&0xFF));
+            offset += 1; retBitOffset += 8;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 2: // 0
+            value = 0;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 3: // not used
+            break;
+        }
+        return value;
+    }
+
+    private static long readBitLongLong(byte[] buf, int off, int bitOff, String name, DecodeCallback cb) {
+        int retBitOffset = 0;
+        int offset = off;
+        int bitOffset = bitOff;
+        byte[] dBuf = new byte[8];
+        long value = 0;
+        
+        byte bitControl = bitOffset==7 ? (byte)((buf[offset]<<1 & 0x02)|(buf[offset+1]>>7 & 0x01)) : (byte)(buf[offset]>>(8-bitOffset-2) & 0x03);
+        bitOffset += 2; retBitOffset += 2;
+        offset += bitOffset/8;
+        bitOffset = bitOffset%8;
+        
+        switch(bitControl) {
+        case 0: // A long (4bytes) follows, little-endian order (LSB first)
+            for (int i=0; i<8; i++) {
+                dBuf[i] = (byte) ((((buf[offset+i+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset+i]<<bitOffset&0xFF));
+            }
+            value = ByteBuffer.wrap(dBuf, 0, 8).order(ByteOrder.LITTLE_ENDIAN).getLong();
+            offset += 8; retBitOffset += 64;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        /*
+        case 1: // An unsgined char (1 byte) follow
+            value = ((((buf[offset+1]&0x00FF)>>(8-bitOffset))&0xFF) | (buf[offset]<<bitOffset&0xFF));
+            offset += 1; retBitOffset += 8;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 2: // 0
+            value = 0;
+            cb.onDecoded(name, value, retBitOffset);
+            break;
+        case 3: // not used
+            break;
+        */
+        }
+        return value;
+    }
+    
+    private static int readRawLong(byte[] buf, int i, int j, String string, DecodeCallback cb) {
         // TODO Auto-generated method stub
-        return null;
+        return 0;
     }
 
     public static int readData(byte[] buf, int offset, Dwg dwg) {
